28640794d1902f4deb98e8875c8e733679fb750e
Kritika Umashankar <kritikaumashankar@gmail.com>
2018-08-30 16:37:39 -0600

    update and destroy

--- app/controllers/post_its_controller.rb
+++ app/controllers/post_its_controller.rb
@@ -1,6 +1,6 @@
 class PostItsController < ApplicationController
   def index
-    @post_its = PostIt.all
+    @post_its = PostIt.order(created_at: :asc)
   end
 
   def show
@@ -20,6 +20,26 @@ class PostItsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @post_it = PostIt.find(params[:id])
+  end
+
+  def update
+    @post_it = PostIt.find(params[:id])
+  
+    if @post_it.update(post_its_params)
+      redirect_to post_its_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    PostIt.find(params[:id]).destroy
+  redirect_to post_its_path
+  end
+
   private
   def post_its_params
     params.require(:post_it).permit(:title, :body, :completed)
